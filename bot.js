d248131befb16b42b436095f6fbb4e5fc9271b56
Anish V <a.vaghasia@utexas.edu>
2016-03-15 15:50:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,11 +18,41 @@ function respond() {
   }
 }
 
+function getInfo(botResponse, options, body, botReq){
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/groups',
+    method: 'GET'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+  
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
 function postMessage() {
   var botResponse, options, body, botReq;
-
+  
   botResponse = "@Seeeaaannn Gajjar"
-
+  getInfo(botResponse, options, body, botReq);
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
