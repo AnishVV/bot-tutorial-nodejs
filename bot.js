d1dcef04a8f0fa20ee3e01bd95dad53c05899c58
Anish V <a.vaghasia@utexas.edu>
2016-03-15 14:42:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "@Anish V"
 
   options = {
     hostname: 'api.groupme.com',
